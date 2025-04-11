085550d823cea41e6a4949f614a31d5f90d0755b
abdo <wevaacademy@gmail.com>
2025-04-11 14:51:30 -0700

    Remove invalid 'disconnect' import from index.js

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -16,13 +16,17 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.DB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log("✅ DB connected successfully");
-    } catch (error) {
-        console.log("❌ DB connection error:", error);
-    }
-};
+  console.log("MongoDB URL:", process.env.DB_URL);
+
+  return await mongoose.connect(process.env.DB_URL).then(res => {
+
+    console.log("✅ DB connected successfully");
+
+
+  }).catch(error => {
+
+    console.log("invalid-db", error);
+
+  })
+
+}
