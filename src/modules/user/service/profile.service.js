bf429ae6bf1f0f323a3fff5255237c6d1621b280
abdo <wevaacademy@gmail.com>
2025-05-25 04:35:54 -0700

    initial commit

--- src/modules/user/service/profile.service.js
+++ src/modules/user/service/profile.service.js
@@ -8,7 +8,7 @@ import { Folder } from "../../../DB/models/foldeer.model.js";
 import File from "../../../DB/models/files.conrroller.js";
 import fs from 'fs';
 import admin from 'firebase-admin';
-import serviceAccount from "../../user/service/notifications.service.json"with { type: 'json' };
+// import serviceAccount from "../../user/service/notifications.service.json"with { type: 'json' };
 import { NotificationModel } from "../../../DB/models/points.model.js";
 
 export const Updateuseraccount = asyncHandelr(async (req, res, next) => {
@@ -493,11 +493,22 @@ export const deleteFolder = asyncHandelr(async (req, res) => {
 
 
 
+
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+        type: process.env.FIREBASE_TYPE,
+        project_id: process.env.FIREBASE_PROJECT_ID,
+        private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+        private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        client_email: process.env.FIREBASE_CLIENT_EMAIL,
+        client_id: process.env.FIREBASE_CLIENT_ID,
+        auth_uri: "https://accounts.google.com/o/oauth2/auth",
+        token_uri: "https://oauth2.googleapis.com/token",
+        auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+        client_x509_cert_url: `https://www.googleapis.com/robot/v1/metadata/x509/${encodeURIComponent(process.env.FIREBASE_CLIENT_EMAIL ?? "")}`,
+    }),
 });
 
-// دالة إرسال إشعار
 async function sendNotification(deviceToken, title, body) {
     const message = {
         notification: { title, body },
